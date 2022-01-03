8d5cf53e443b9e33e0d4e6aa9eae6d50ea64a142
Harshit <harshitsinghchouhan2018@gmail.com>
2022-01-03 16:39:59 +0530

    profile update and discard button added

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./profile.scss";
 
 import Avatar1 from "./../../assets/profile/avatar-1.svg";
+import CustomButton from "../../components/atmoic/customButton/customButton";
 
 const Profile = () => {
+  const [update, setUpdate] = useState(false);
+
+  const handleUpdate = () => setUpdate(true);
+  const handleSave = () => setUpdate(false);
+  const handleDiscard = () => setUpdate(false);
+
+  const updateButtonAttributes = {
+    type: "button",
+    text: "Update Profile",
+    classes: "font-weight-bold",
+    onClick: handleUpdate,
+  };
+
+  const saveButtonAttributes = {
+    type: "button",
+    text: "Save Changes",
+    classes: "font-weight-bold mr-3",
+    onClick: handleSave,
+  };
+
+  const discardButtonAttributes = {
+    type: "button",
+    text: "Discard Changes",
+    classes: "font-weight-bold red-btn",
+    onClick: handleDiscard,
+  };
+
   return (
     <div className="container profilePage">
       <div className="row">
@@ -12,7 +40,7 @@ const Profile = () => {
           <h2 className="profilePage__title">Your Profile</h2>
         </div>
 
-        <div className="col-12 px-5 px-lg-0 profilePage__box">
+        <div className="col-12 px-5 pl-lg-3 profilePage__box">
           <div className="row">
             <div className="col-lg-3 mb-5 mb-lg-0 d-flex align-items-start justify-content-center">
               <div className="profilePage__image">
@@ -25,13 +53,15 @@ const Profile = () => {
                   <label htmlFor="firstName" className="profilePage__box--label">
                     First Name
                   </label>
-                  <div className="profilePage__box--value">Harshit</div>
+                  {!update && <div className="profilePage__box--value">Harshit</div>}
+                  {update && <input type="text" value="Harshit" className="form-control formControl-input" />}
                 </div>
                 <div className="col-md-6 mt-4 mt-md-0">
                   <label htmlFor="firstName" className="profilePage__box--label">
                     Last Name
                   </label>
-                  <div className="profilePage__box--value">Chouhan</div>
+                  {!update && <div className="profilePage__box--value">Chouhan</div>}
+                  {update && <input type="text" value="Chouhan" className="form-control formControl-input" />}
                 </div>
               </div>
 
@@ -46,7 +76,8 @@ const Profile = () => {
                   <label htmlFor="firstName" className="profilePage__box--label">
                     Mobile Number
                   </label>
-                  <div className="profilePage__box--value">+91 8107136147</div>
+                  {!update && <div className="profilePage__box--value">+91 8107136147</div>}
+                  {update && <input type="text" value="+91 8107136147" className="form-control formControl-input" />}
                 </div>
               </div>
 
@@ -55,13 +86,15 @@ const Profile = () => {
                   <label htmlFor="firstName" className="profilePage__box--label">
                     State
                   </label>
-                  <div className="profilePage__box--value">Rajasthan</div>
+                  {!update && <div className="profilePage__box--value">Rajasthan</div>}
+                  {update && <input type="text" value="Rajasthan" className="form-control formControl-input" />}
                 </div>
                 <div className="col-md-6 mt-4 mt-md-0">
                   <label htmlFor="firstName" className="profilePage__box--label">
                     City
                   </label>
-                  <div className="profilePage__box--value">Pali</div>
+                  {!update && <div className="profilePage__box--value">Pali</div>}
+                  {update && <input type="text" value="Pali" className="form-control formControl-input" />}
                 </div>
               </div>
 
@@ -70,7 +103,18 @@ const Profile = () => {
                   <label htmlFor="firstName" className="profilePage__box--label">
                     Address
                   </label>
-                  <div className="profilePage__box--value">1 - D - 23 New Housing Board, Pali (Marwar), Rajasthan, 306401</div>
+                  {!update && <div className="profilePage__box--value">1 - D - 23 New Housing Board, Pali (Marwar), Rajasthan, 306401</div>}
+                  {update && (
+                    <textarea value="1 - D - 23 New Housing Board, Pali (Marwar), Rajasthan, 306401" className="form-control formControl-input" />
+                  )}
+                </div>
+              </div>
+
+              <div className="row mt-3">
+                <div className="col-12 d-flex align-items-center justify-content-end">
+                  {!update && <CustomButton {...updateButtonAttributes} />}
+                  {update && <CustomButton {...saveButtonAttributes} />}
+                  {update && <CustomButton {...discardButtonAttributes} />}
                 </div>
               </div>
             </div>
