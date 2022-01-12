960db89623c0d68eb8cc18397c95438dff763652
Medusa <kawthekar56@gmail.com>
2022-01-12 19:05:03 +0530

    vendor home

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,7 +4,7 @@ import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Profile from "../pages/profile/profile";
 import Services from "../pages/services/services";
 import Home from "./../pages/home/home";
-
+import Homepage from "./../pages/vendor/homePage";
 const Routes = () => {
   return (
     <BrowserRouter>
@@ -12,6 +12,7 @@ const Routes = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/services" component={Services} />
+        <Route exact path="/vendorhome" component={Homepage} />
       </Switch>
     </BrowserRouter>
   );
