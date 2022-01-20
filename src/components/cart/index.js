9e1c2a63e26514f79e04950ac717b99c11a75602
Harshit <harshitsinghchouhan2018@gmail.com>
2022-01-20 09:23:21 +0530

    cart popup added

---
+++ src/components/cart/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./cart.scss";
+
+const Cart = () => {
+  return <div className="cart">Services</div>;
+};
+
+export default Cart;
--- src/pages/services/services.js
+++ src/pages/services/services.js
@@ -9,6 +9,7 @@ import VendorTile from "../../components/vendorTile/vendorTile";
 import { catchHandler } from "./../../utlis/catchHandler.utlis";
 import { ApiCallsContext } from "../../services/api.service";
 import { API_URLS } from "../../utlis/constants";
+import Cart from "../../components/cart";
 
 const Services = () => {
   const ApiContext = useContext(ApiCallsContext);
@@ -42,41 +43,44 @@ const Services = () => {
   };
 
   return (
-    <div className="services">
-      <div className="services__banner">
-        <img src={bannerPic} alt="Banner" />
-        <h2 className="services__banner--text">{name} Services</h2>
-      </div>
-      <div className="services__listing-container container">
-        <div className="row">
-          <div className="col-4">
-            <div className="services__filter-box services__filter-box--1">
-              <div className="services__filter-box--title">Sort By</div>
-              <div className="services__filter-box--options">Price: High to Low</div>
-              <div className="services__filter-box--options">Price: Low to High</div>
-              <div className="services__filter-box--options">Rating: High to Low</div>
-              <div className="services__filter-box--options">Rating: Low to High</div>
-            </div>
+    <>
+      <div className="services">
+        <div className="services__banner">
+          <img src={bannerPic} alt="Banner" />
+          <h2 className="services__banner--text">{name} Services</h2>
+        </div>
+        <div className="services__listing-container container">
+          <div className="row">
+            <div className="col-4">
+              <div className="services__filter-box services__filter-box--1">
+                <div className="services__filter-box--title">Sort By</div>
+                <div className="services__filter-box--options">Price: High to Low</div>
+                <div className="services__filter-box--options">Price: Low to High</div>
+                <div className="services__filter-box--options">Rating: High to Low</div>
+                <div className="services__filter-box--options">Rating: Low to High</div>
+              </div>
 
-            <div className="services__filter-box services__filter-box--2 mt-4">
-              <div className="services__filter-box--title">Price Range</div>
-              <div className="d-flex align-items-center justify-content-between mt-3">
-                <div className="d-flex align-items-center justify-content-between flex-grow-1 mr-3">
-                  <input type="number" placeholder="MIN" className="form-control range-input" />
-                  <span>TO</span>
-                  <input type="number" placeholder="MAX" className="form-control range-input" />
+              <div className="services__filter-box services__filter-box--2 mt-4">
+                <div className="services__filter-box--title">Price Range</div>
+                <div className="d-flex align-items-center justify-content-between mt-3">
+                  <div className="d-flex align-items-center justify-content-between flex-grow-1 mr-3">
+                    <input type="number" placeholder="MIN" className="form-control range-input" />
+                    <span>TO</span>
+                    <input type="number" placeholder="MAX" className="form-control range-input" />
+                  </div>
+                  <CustomButton {...buttonAttributes} />
                 </div>
-                <CustomButton {...buttonAttributes} />
               </div>
             </div>
-          </div>
-          <div className="col-8">
-            {vendors.length > 0 && vendors.map((vendor, index) => <VendorTile key={index} {...vendor} serviceId={serviceId} />)}
-            {vendors.length <= 0 && <h2 className="text-center">Sorry, No vendor found</h2>}
+            <div className="col-8">
+              {vendors.length > 0 && vendors.map((vendor, index) => <VendorTile key={index} {...vendor} serviceId={serviceId} />)}
+              {vendors.length <= 0 && <h2 className="text-center">Sorry, No vendor found</h2>}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Cart />
+    </>
   );
 };
 
