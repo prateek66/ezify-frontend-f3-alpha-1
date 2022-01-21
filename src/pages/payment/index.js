7c59c872aa913a505fba8bef52c4d31ee44f6276
Harshit <harshitsinghchouhan2018@gmail.com>
2022-01-21 23:02:25 +0530

    login check done on payment window

--- src/pages/payment/index.js
+++ src/pages/payment/index.js
@@ -15,26 +15,37 @@ import { catchHandler } from "../../utlis/catchHandler.utlis";
 import { API_URLS } from "../../utlis/constants";
 import { setToasterConfig } from "../../redux/toaster/toaster.actions";
 import { withRouter } from "react-router-dom";
+import { Modal } from "react-bootstrap";
+import ModalBase from "../../components/atmoic/modal/modal";
 
-const Payment = ({ cartItems, totalAmount, disabledItemFromCart, userToken, userDetails, setToasterCofig, history }) => {
+const Payment = ({ cartItems, totalAmount, disabledItemFromCart, userToken, setToasterCofig, history }) => {
   const ApiContext = useContext(ApiCallsContext);
 
-  const handlePaymentSubmit = async () => {
-    if (userToken) {
-      const response = await catchHandler(paymentSubmitAPI);
-      console.log(response);
-      if (response) {
-        setToasterCofig({
-          show: true,
-          message: "Payment done successfully",
-          className: "success",
-        });
+  const [show, setShow] = useState(false);
+  const [callAPI, setCallAPI] = useState(false);
 
-        setTimeout(() => {
-          history.push("/bookings");
-        }, 3000);
-      }
+  const handleClose = () => {
+    setShow(false);
+    setCallAPI(false);
+  };
+  const handleShow = () => setShow(true);
+
+  const handlePaymentSubmit = async () => {
+    const response = await catchHandler(paymentSubmitAPI);
+    console.log(response);
+    if (response) {
+      setToasterCofig({
+        show: true,
+        message: "Payment done successfully",
+        className: "success",
+      });
+
+      setTimeout(() => {
+        history.push("/bookings");
+      }, 3000);
     }
+
+    console.log(userToken);
   };
 
   const paymentSubmitAPI = async () => {
@@ -65,7 +76,7 @@ const Payment = ({ cartItems, totalAmount, disabledItemFromCart, userToken, user
     type: "submit",
     text: "PAY NOW",
     classes: "cp-3 btn-block",
-    onClick: handlePaymentSubmit,
+    onClick: () => setCallAPI(true),
   });
 
   useEffect(() => {
@@ -75,7 +86,7 @@ const Payment = ({ cartItems, totalAmount, disabledItemFromCart, userToken, user
         text: "PAY NOW",
         classes: "cp-3 btn-block",
         disabled: true,
-        onClick: handlePaymentSubmit,
+        onClick: () => setCallAPI(true),
       });
     } else {
       setbtnAttributes({
@@ -83,71 +94,90 @@ const Payment = ({ cartItems, totalAmount, disabledItemFromCart, userToken, user
         text: "PAY NOW",
         classes: "cp-3 btn-block",
         disabled: false,
-        onClick: handlePaymentSubmit,
+        onClick: () => setCallAPI(true),
       });
     }
   }, [totalAmount]);
 
+  useEffect(() => {
+    if (callAPI) {
+      if (userToken) {
+        handlePaymentSubmit();
+      } else {
+        setShow(true);
+      }
+    }
+  }, [userToken, callAPI]);
+
   return (
-    <div className="payment-page px-3 px-lg-0">
-      <div className="container">
-        <div className="row py-4">
-          <div className="col-9 mx-auto payment-page__heading-text py-2">PAYMENT</div>
-          <div className="col-lg-9 col-md-11 payment-page__custom-container mx-auto">
-            <div className="payment-page__custom-container-1 customScroll">
-              {cartItems.map((item, index) => (
-                <div className="row payment-page__vendorTile mb-2 mr-2" key={index}>
-                  <div className={`col-2 d-flex align-items-center justify-content-center ${item.active ? null : "custom-blur"}`}>
-                    <img src={item.profileImage} alt="Vendor Profile" className="h-100 w-100 rounded-circle" />
-                  </div>
-                  <div className={`col-4 ${item.active ? null : "custom-blur"}`}>
-                    <div className="mb-2 d-flex align-items-center justify-content-start payment-page__vendorTile__icons">{item.serviceName}</div>
-                    <div className="d-flex align-items-center justify-content-between payment-page__vendorTile__nameRatings">
-                      <div className="payment-page__vendorTile__name">
-                        {item.firstName} {item.lastName}
+    <>
+      <div className="payment-page px-3 px-lg-0">
+        <div className="container">
+          <div className="row py-4">
+            <div className="col-9 mx-auto payment-page__heading-text py-2">PAYMENT</div>
+            <div className="col-lg-9 col-md-11 payment-page__custom-container mx-auto">
+              <div className="payment-page__custom-container-1 customScroll">
+                {cartItems.map((item, index) => (
+                  <div className="row payment-page__vendorTile mb-2 mr-2" key={index}>
+                    <div className={`col-2 d-flex align-items-center justify-content-center ${item.active ? null : "custom-blur"}`}>
+                      <img src={item.profileImage} alt="Vendor Profile" className="h-100 w-100 rounded-circle" />
+                    </div>
+                    <div className={`col-4 ${item.active ? null : "custom-blur"}`}>
+                      <div className="mb-2 d-flex align-items-center justify-content-start payment-page__vendorTile__icons">{item.serviceName}</div>
+                      <div className="d-flex align-items-center justify-content-between payment-page__vendorTile__nameRatings">
+                        <div className="payment-page__vendorTile__name">
+                          {item.firstName} {item.lastName}
+                        </div>
+                      </div>
+                      <div className="d-flex align-items-center justify-content-start payment-page__vendorTile__ratings">
+                        <img src={ratingStar} alt="Rating" />
+                        <span>4.5</span>
                       </div>
                     </div>
-                    <div className="d-flex align-items-center justify-content-start payment-page__vendorTile__ratings">
-                      <img src={ratingStar} alt="Rating" />
-                      <span>4.5</span>
+                    <div className="col-3 offset-3 d-flex align-items-end justify-content-between flex-column">
+                      <div className="">
+                        <input
+                          type="checkbox"
+                          className="custom-toggle-btn"
+                          checked={item.active}
+                          onChange={() => disabledItemFromCart(item.serviceID)}
+                        />
+                      </div>
+                      <div className="d-flex align-items-center justify-content-between payment-page__vendorTile__pricing">
+                        <img src={rupee} alt="Rupee Symbol" />
+                        <span className="ml-2">{item.basePrice}</span>
+                      </div>
                     </div>
                   </div>
-                  <div className="col-3 offset-3 d-flex align-items-end justify-content-between flex-column">
-                    <div className="">
-                      <input
-                        type="checkbox"
-                        className="custom-toggle-btn"
-                        checked={item.active}
-                        onChange={() => disabledItemFromCart(item.serviceID)}
-                      />
-                    </div>
-                    <div className="d-flex align-items-center justify-content-between payment-page__vendorTile__pricing">
-                      <img src={rupee} alt="Rupee Symbol" />
-                      <span className="ml-2">{item.basePrice}</span>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
+                ))}
+              </div>
 
-            <div className="col-12 d-flex align-items-end justify-content-between flex-column mb-3">
-              <div className="payment-page__final-pricing">
-                <span className="">
-                  <b>Total</b>
-                </span>
+              <div className="col-12 d-flex align-items-end justify-content-between flex-column mb-3">
+                <div className="payment-page__final-pricing">
+                  <span className="">
+                    <b>Total</b>
+                  </span>
+                </div>
+                <div className="d-flex align-items-center justify-content-between payment-page__vendorTile__pricing payment-page__final-pricing">
+                  <img src={rupee} alt="Rupee Symbol" />
+                  <span className="ml-2">{totalAmount}</span>
+                </div>
               </div>
-              <div className="d-flex align-items-center justify-content-between payment-page__vendorTile__pricing payment-page__final-pricing">
-                <img src={rupee} alt="Rupee Symbol" />
-                <span className="ml-2">{totalAmount}</span>
+              <div className="col-12 d-flex align-items-center justify-content-center">
+                <CustomButton {...btnAttributes} />
               </div>
-            </div>
-            <div className="col-12 d-flex align-items-center justify-content-center">
-              <CustomButton {...btnAttributes} />
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <ModalBase show={show} handleClose={handleClose} handleShow={handleShow} size="md">
+        <Modal.Header closeButton>💥 Warning</Modal.Header>
+        <Modal.Body>
+          <p className="mb-0 text-center font-weight-bold">Please Login</p>
+        </Modal.Body>
+      </ModalBase>
+    </>
   );
 };
 
